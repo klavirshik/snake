f0949729c7c8c3dd047ad597f6781cf7809746b1
klavirshik <klavirshik@yandex.ru>
2016-11-20 14:20:49 +0800

    color

---
+++ Snake/Snake/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Food:Point
+    {
+        public Food(int _x, int _y, char _sym)
+            :base(_x, _y, _sym)
+        {
+                 }
+        public override void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Draw();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
